a3d51d79dbf79d0ec1350a62b9ec2c76e4c4ed6f
gilkom <gilek06@op.pl>
2019-08-09 10:27:57 +0200

    Adding model class Orders and Orders_Details

--- InvoiceGenerator/src/model/Orders.java
+++ InvoiceGenerator/src/model/Orders.java
@@ -1,5 +1,56 @@
 package model;
 
 public class Orders {
-
+	private int orderId;
+	private int customerId;
+	private String orderDate;
+	private String invoiceDate;
+	private String orderTotal;
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate= orderDate;
+	}
+	public String getInvoiceDate() {
+		return invoiceDate;
+	}
+	public void setInvoiceDate(String invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+	public String getOrderTotal() {
+		return orderTotal;
+	}
+	public void setOrderTotal(String orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+	
+	public Orders() {};
+	public Orders(int orderId, int customerId, String orderDate,
+					String invoiceDate, String orderTotal) {
+		this.orderId = orderId;
+		this.customerId = customerId;
+		this.orderDate = orderDate;
+		this.invoiceDate = invoiceDate;
+		this.orderTotal = orderTotal;
+	}
+	
+	@Override
+	public String toString() {
+		return "[" + orderId + "] customer id: " + customerId + ", Order date: " + 
+				orderDate + ", Invoice date: " + invoiceDate + ", Total: " + orderTotal;
+	}
 }
--- InvoiceGenerator/src/model/Orders_Details.java
+++ InvoiceGenerator/src/model/Orders_Details.java
@@ -1,5 +1,66 @@
 package model;
 
 public class Orders_Details {
-
+	private int orderId;
+	private int itemNumber;
+	private int productId;
+	private double purchasePrice;
+	private int itemQuantity;
+	private double itemTotal;
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getItemNumber() {
+		return itemNumber;
+	}
+	public void setItemNumber(int itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+	public double getItemTotal() {
+		return itemTotal;
+	}
+	public void setItemTotal(double itemTotal) {
+		this.itemTotal = itemTotal;
+	}
+	
+	public Orders_Details() {};
+	public Orders_Details(int orderId, int itemNumber, int productId, double purchasePrice,
+							int itemQuantity, double itemTotal) {
+		this.orderId = orderId;
+		this.itemNumber = itemNumber;
+		this.productId = productId;
+		this.purchasePrice = purchasePrice;
+		this.itemQuantity = itemQuantity;
+		this.itemTotal = itemTotal;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order[" + orderId + "], Item[" + itemNumber + "], product[" +
+				productId + "], Price: " + purchasePrice + ", quantity: " +
+				itemQuantity + ", Item total: " + itemTotal;
+	}
+	
 }
